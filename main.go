14a1740d21d1e91cabfe87c20f911017296b2404
SarikViper <dzamolovsardor5@gmail.com>
2025-04-30 21:48:35 +0500

    -

--- main.go
+++ main.go
@@ -29,55 +29,132 @@
 // 	}
 // }
 
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"log"
+// 	"net/http"
+// )
+
+// type Thread struct {
+// 	ID        int    `json:"id"`
+// 	Avatar    string `json:"avatar"`
+// 	User      string `json:"user"`
+// 	Time      string `json:"time"`
+// 	Text      string `json:"text"`
+// 	LikeCount int    `json:"likeCount"`
+// 	Comments  int    `json:"comments"`
+// 	Resends   int    `json:"resends"`
+// 	Views     int    `json:"views"`
+// }
+
+// var threads = []Thread{
+// 	{	1, "/danya.jpg", "Danya", "5ч", "My name is Danya, I am a Frontend Developer", 200, 200, 200, 200},
+// 	{2, "/sardor.jpg", "Sardor", "2ч", "My name is Sardor, I am a Frontend Developer", 500, 245, 464, 654},
+// 	{3, "/avatar.jpg", "Erkin", "1ч", "Hello world, it's Erkin here! Writing some code 🚀", 312, 98, 112, 430},
+// 	{4, "/avatar.jpg", "Murodjon", "3ч", "Building something cool with React and Next.js 🔥", 220, 150, 133, 512},
+// 	{5, "/avatar.jpg", "Ramizjon", "6ч", "Just finished my new portfolio website 🎉", 610, 342, 278, 800},
+// 	{6, "/avatar.jpg", "Daler", "8ч", "Working on some TypeScript magic ✨", 180, 76, 90, 300},
+// 	{7, "/avatar.jpg", "Ozodbek", "9ч", "I love using shadcn/ui in my projects!", 400, 210, 150, 550},
+// 	{8, "/avatar.jpg", "Konstantin", "10ч", "Frontend is fun when you understand the logic 🧠", 275, 115, 134, 460},
+// 	{9, "/avatar.jpg", "Shaxriyor", "11ч", "CSS can be tricky, but Tailwind makes it easier 🧩", 490, 198, 205, 620},
+// 	{10, "/avatar.jpg", "Alex", "12ч", "Sharing my open-source project soon, stay tuned 💡", 305, 150, 160, 580},
+// }
+
+// func threadsHandler(w http.ResponseWriter, r *http.Request) {
+// 	if r.Method == http.MethodGet {
+// 		w.Header().Set("Content-Type", "application/json")
+// 		json.NewEncoder(w).Encode(threads)
+// 		return
+// 	}
+
+// 	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+// }
+
+// func main() {
+// 	http.HandleFunc("/api/threads", threadsHandler)
+// 	log.Println("Server running on http://localhost:4000")
+// 	log.Fatal(http.ListenAndServe(":4000", nil))
+// }
+
+// func Hello() {
+// 	print("Hello, World!")
+// }
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var name string = "Sardor"
+// 	var age int = 15
+// 	var isFrontend bool = true
+// 	city := "Samarkand"
+// 	var surname string
+// 	fmt.Println("Введите вашу фамилию")
+// 	fmt.Scan(&surname)
+// 	fmt.Println("Ваша фамилия:", surname)
+// 	fmt.Println("Ваше имя:", name)
+// 	fmt.Println("Ваш возраст:", age)
+// 	fmt.Println("Вы фронтендер", isFrontend)
+// 	fmt.Println("Ваш город", city)
+
+// 	if age < 18 {
+// 		fmt.Println("Вы еще ребенок")
+// 	} else {
+// 		fmt.Println("Вы уже взрослый")
+// 	}
+
+// 	if isFrontend == true {
+// 		fmt.Println("Ты програмист")
+// 	}else {
+// 		fmt.Println("Ты не програмист")
+
+// 	}
+// }
+
 package main
 
-import (
-	"encoding/json"
-	"log"
-	"net/http"
-)
-
-type Thread struct {
-	ID        int    `json:"id"`
-	Avatar    string `json:"avatar"`
-	User      string `json:"user"`
-	Time      string `json:"time"`
-	Text      string `json:"text"`
-	LikeCount int    `json:"likeCount"`
-	Comments  int    `json:"comments"`
-	Resends   int    `json:"resends"`
-	Views     int    `json:"views"`
-}
-
-var threads = []Thread{
-	{	1, "/danya.jpg", "Danya", "5ч", "My name is Danya, I am a Frontend Developer", 200, 200, 200, 200},
-	{2, "/sardor.jpg", "Sardor", "2ч", "My name is Sardor, I am a Frontend Developer", 500, 245, 464, 654},
-	{3, "/avatar.jpg", "Erkin", "1ч", "Hello world, it's Erkin here! Writing some code 🚀", 312, 98, 112, 430},
-	{4, "/avatar.jpg", "Murodjon", "3ч", "Building something cool with React and Next.js 🔥", 220, 150, 133, 512},
-	{5, "/avatar.jpg", "Ramizjon", "6ч", "Just finished my new portfolio website 🎉", 610, 342, 278, 800},
-	{6, "/avatar.jpg", "Daler", "8ч", "Working on some TypeScript magic ✨", 180, 76, 90, 300},
-	{7, "/avatar.jpg", "Ozodbek", "9ч", "I love using shadcn/ui in my projects!", 400, 210, 150, 550},
-	{8, "/avatar.jpg", "Konstantin", "10ч", "Frontend is fun when you understand the logic 🧠", 275, 115, 134, 460},
-	{9, "/avatar.jpg", "Shaxriyor", "11ч", "CSS can be tricky, but Tailwind makes it easier 🧩", 490, 198, 205, 620},
-	{10, "/avatar.jpg", "Alex", "12ч", "Sharing my open-source project soon, stay tuned 💡", 305, 150, 160, 580},
-}
-
-func threadsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(threads)
-		return
-	}
-
-	http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/api/threads", threadsHandler)
-	log.Println("Server running on http://localhost:4000")
-	log.Fatal(http.ListenAndServe(":4000", nil))
-}
+	var name string
+	fmt.Print("What's your name? ")
+	fmt.Scan(&name)
 
-func Hello() {
-	print("Hello, World!")
+	var age int
+	fmt.Print("Enter your age ")
+	fmt.Scan(&age)
+	fmt.Println("Your name is:", name)
+	fmt.Println("You are:", age, "years old")
+
+	if age < 14 {
+		fmt.Println("You are still a kid")
+	} else if age > 14 && age < 18 {
+		fmt.Println("You are teen")
+	} else {
+		fmt.Println("You are adult")
+	}
+
+
+	// Обычный цикл ничего не обычного
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+
+	// Цикл как while или женская логика сначало делать потом думать
+	i := 0
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+
+	// Unlimited cycle && infiniy (не делать так ломает комп)
+	// for{
+	// 	print("Не делать так пожалуйста или комп взорвется просто знай про это закоментируй и забудь")
+	// }
+	
+
 }
